b4174dcad41e5d880399a72dcb22b2bbdc9e0a6f
bruce11521 <bruce11521@gmail.com>
2019-10-04 21:29:37 +0800

    Initial Commit

---
+++ Git Tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git Tutorial
+//
+//  Created by 林伯翰 on 2019/10/4.
+//  Copyright © 2019 林伯翰. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
